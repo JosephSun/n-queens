bb657351a3121041416155ab42072bbb2eab3307
BKhurshid <josephsun4@gmail.com>
2015-10-23 15:12:20 -0700

    Pseudo code added to rook Solution

--- src/solvers.js
+++ src/solvers.js
@@ -3,13 +3,10 @@
   / __|/ _ \| \ \ / / _ \ '__/ __|
   \__ \ (_) | |\ V /  __/ |  \__ \
   |___/\___/|_| \_/ \___|_|  |___/
-
 */
-
 // hint: you'll need to do a full-search of all possible arrangements of pieces!
 // (There are also optimizations that will allow you to skip a lot of the dead search space)
 // take a look at solversSpec.js to see what the tests are expecting
-
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 // var matrixXx = [
 //       [0, 1, 0, 0],
@@ -20,33 +17,21 @@
 // var board = new Board(matrixXx);
 // console.log("board", board,"board.hasAnyMajorDiagonalConflicts()",board.hasAnyMajorDiagonalConflicts())
 window.findNRooksSolution = function(n) {
-  var solution = new makeEmptyMatrix(n); //fixme
-
+  var solution = new makeEmptyMatrix(n); //makes an empty array
     var arrPosition=[];//create an array with n elements
-    //arrPosition shuold be [0,1,2,3,4,5....n-1]
-    for (var k = 0; k < n; k++) {
+    for (var k = 0; k < n; k++) {    //arrPosition shuold be [0,1,2,3,4,5....n-1]
       arrPosition.push(k);
     }
-    var j = 0
+    var j = 0;
     for(var i=n; i > 0; i--) {
-      var rookPos = arrPosition[Math.floor(Math.random() * i)];
+      var rookPos = arrPosition[Math.floor(Math.random() * i)];//check random position
       solution[rookPos][j] = 1;    //pushes random position in incrementing rows with each loop
       arrPosition.splice(arrPosition.indexOf(rookPos),1); //finds position of rookPos in arrPosition array, then splice it out
       j++;
     }
-
-    
     // console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
     return solution;
-
-
 };
-  
-
-
-
-
-
 
   var makeEmptyMatrix = function(n) {
     return _(_.range(n)).map(function() {
@@ -55,10 +40,8 @@ window.findNRooksSolution = function(n) {
       });
     });
   };
-
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-
   var solutionCount = 1; //fixme
       // rook solution, is the same as number as this.attributes.n   i.e. 5x5 grid contains 5 total. 
   for(var i=n; i > 0; i--) {
@@ -70,11 +53,8 @@ window.countNRooksSolutions = function(n) {
   //  console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 };
-
-
 window.findSolution = function(row, n, board, validator, callback) {
   //if all rows exhausted
-
   if (row === n) {
     return callback();
   }
@@ -92,15 +72,9 @@ window.findSolution = function(row, n, board, validator, callback) {
     // unplace a piece
     board.togglePiece(row, i);
   }
-
-
 };
-
-
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
-  
-
 //One attempted Solution
   /*var holdingN =n;
   // if (n == 0) {
@@ -160,12 +134,9 @@ window.findNQueensSolution = function(n) {
 //   console.log("solution.attributes from solvers file", solution.attributes);
 //   return solution.attributes;
 };
-
-
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
   var solutionCount = undefined; //fixme
-
   // console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
 };
